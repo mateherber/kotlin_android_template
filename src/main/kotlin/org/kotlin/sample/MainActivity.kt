6a614a248e50f110fc2658e9e76d4f9cede34814
Máté Herber <mate.herber@skyscanner.net>
2017-04-04 15:53:27 +0200

    Enabled coroutines

--- src/main/kotlin/org/kotlin/sample/MainActivity.kt
+++ src/main/kotlin/org/kotlin/sample/MainActivity.kt
@@ -2,10 +2,15 @@ package org.kotlin.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        launch(UI) {
+
+        }
     }
 }
