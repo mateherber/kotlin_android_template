da2bf8b33b5572910ece487282782a339ed62d69
Máté Herber <mate.herber@skyscanner.net>
2017-04-18 16:01:11 +0200

    Added libraries

--- src/main/kotlin/org/kotlin/sample/MainActivity.kt
+++ src/main/kotlin/org/kotlin/sample/MainActivity.kt
@@ -1,16 +1,11 @@
 package org.kotlin.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        launch(UI) {
-
-        }
     }
 }
